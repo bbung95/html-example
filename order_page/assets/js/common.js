dfe93db1475b04888255b49b502cc59f42dd4640
bbung95 <tjd123123@naver.com>
2022-12-15 18:38:18 +0900

    feature : order-page review-area orderer-img-list review-list

--- order_page/assets/js/common.js
+++ order_page/assets/js/common.js
@@ -1,2 +1,2 @@
 document.querySelectorAll(".badge-cart").forEach((item) => item.addEventListener("click", () => (location.href = "#")));
-document.querySelectorAll(".link-back").forEach((item) => item.addEventListener("click", () => history.back()));
+document.querySelectorAll(".link-back").forEach((item) => item.addEventListener("click", () => (location.href = "./index.html")));
